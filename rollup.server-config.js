2c32d86dd30676bdbabe366ca0a8262d1087c28a
adibas03 <adibas03@ymail.com>
2024-10-25 01:09:20 +0100

    timezone prefill country

--- rollup.server-config.js
+++ rollup.server-config.js
@@ -1,17 +1,20 @@
-import riot from 'rollup-plugin-riot'
-import typescript from '@rollup/plugin-typescript'
+import riot from "rollup-plugin-riot";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/app.riot',
-  external: ['@riotjs/hydrate', 'erre', '@riotjs/route', 'riot'],
+  input: "src/app.riot",
+  external: ["@riotjs/hydrate", "erre", "@riotjs/route", "riot", "timezonedb"],
   output: {
-    file: 'public/app.js',
-    format: 'esm',
+    file: "public/app.js",
+    format: "esm",
+  },
+  paths: {
+    timezonedb: "./src/data/time_zone.csv",
   },
   plugins: [
     riot(),
     typescript({
-      include: "./src/**"
-    })
-  ]
-}
+      include: "./src/**",
+    }),
+  ],
+};
